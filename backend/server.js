ca435e614fbbdba2da59d5afcd797ff6a6d1b62a
andgertsen12 <andgertsen@gmail.com>
2025-03-21 10:44:40 +0100

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -9,6 +9,10 @@ app.set("view engine", "ejs");
 app.use(express.static("public"));
 app.use(bodyParser.urlencoded({ extended: true }));
 
+app.get("/dashboard", (req, res) => {
+  res.render("dashboard"); // Renderer "views/dashboard.ejs"
+});
+
 app.listen(PORT, () => {
   console.log(`Serveren kører på http://localhost:${PORT}`);
 });
