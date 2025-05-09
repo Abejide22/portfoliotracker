490a2bad84d2d967aa4b0000d35684a472acd23d
Awaisfazil <awaisdx10@icloud.com>
2025-05-10 01:40:22 +0200

    komm

--- backend/server.js
+++ backend/server.js
@@ -1,18 +1,18 @@
-const express = require("express");
-const bodyParser = require("body-parser");
-const path = require("path");
+const express = require("express"); // Henter express frameworket
+const bodyParser = require("body-parser"); // Bruges til at læse data fra formularer
+const path = require("path"); // Bruges til at finde stien til filer
 const session = require("express-session"); // vi tilføjer express-session for bl.a. at forhindre folk i at tilgå sider uden at være logget ind
-const { pool, poolConnect, sql } = require("./database/database");
-const routes = require("./routes/all_routes");
+const { pool, poolConnect, sql } = require("./database/database"); // Importerer databaseforbindelsen
+const routes = require("./routes/all_routes"); // Importerer ruterne
 
-const app = express();
-const PORT = 3000;
+const app = express(); // Opretter express sereren
+const PORT = 3000; // Definerer porten serveren skal køre på
 
-app.set("view engine", "ejs");
-app.use(express.static(path.join(__dirname, "../public")));
-app.use(bodyParser.urlencoded({ extended: true }));
+app.set("view engine", "ejs"); // Sætter EJS som templating engine
+app.use(express.static(path.join(__dirname, "../public"))); // Sætter public mappen hvor statiske filer ligger
+app.use(bodyParser.urlencoded({ extended: true })); // Gør det muligt at læse data fra formularer
 app.use(express.json()); // Tilføjer denne linje for at parse JSON-data
-app.set("views", path.join(__dirname, "../views"));
+app.set("views", path.join(__dirname, "../views")); // Fortæller hvor ejs filerne ligger
 
 app.use(session({
   secret: 'Løverne', // En hemmelig nøgle til at kryptere sessionen.
@@ -20,8 +20,8 @@ app.use(session({
   saveUninitialized: false
 }));
 
-app.use("/", routes);
+app.use("/", routes); // Bruger ruterne fra all_routes.js
 
 app.listen(PORT, () => {
-  console.log(`Serveren kører på http://localhost:${PORT}`);
+  console.log(`Serveren kører på http://localhost:${PORT}`); // Starter selve serveren 
 });
