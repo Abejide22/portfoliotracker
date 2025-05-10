7f8ce538e67024b38ffdcdf7e9e1d095383c4fa1
Awaisfazil <awaisdx10@icloud.com>
2025-05-11 01:13:34 +0200

    komm

--- backend/routes/navigation_routes.js
+++ backend/routes/navigation_routes.js
@@ -1,21 +1,22 @@
-const express = require("express");
-const router = express.Router();
-const { pool, poolConnect, sql } = require("../database/database");
+const express = require("express"); // Henter express frameworket
+const router = express.Router(); // Opretter en router
+const { pool, poolConnect, sql } = require("../database/database"); // Importerer databaseforbindelsen
 const fs = require("fs");
 const request = require("request");
 
-router.use(express.urlencoded({ extended: true }));
+router.use(express.urlencoded({ extended: true })); // Bruges til at læse data fra formularer
 
 // Navigation-Route
 
+//
 router.get("/", (req, res) => {
-    res.redirect("/index");
-  });
-  
+  res.redirect("/index");
+});
+
 
 router.get("/index", (req, res) => {
-    res.render("index");
-  });
+  res.render("index");
+});
 
 module.exports = router;
 
